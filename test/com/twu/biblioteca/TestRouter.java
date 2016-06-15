ab824d2ebe906448afc214fda0c57503453c2952
linger <llf_hwj@163.com>
2016-06-15 16:32:35 +0800

    commit testRouter and TestUserRepository

--- test/com/twu/biblioteca/TestRouter.java
+++ test/com/twu/biblioteca/TestRouter.java
@@ -1,14 +1,15 @@
 package com.twu.biblioteca;
 
 import com.twu.biblioteca.model.Book;
-import com.twu.biblioteca.myCommandImpl.CheckoutBook;
-import com.twu.biblioteca.myCommandImpl.ListBooks;
-import com.twu.biblioteca.myCommandImpl.Quit;
-import com.twu.biblioteca.myCommandImpl.ReturnBook;
+import com.twu.biblioteca.model.Movie;
+import com.twu.biblioteca.model.User;
+import com.twu.biblioteca.myCommandImpl.*;
 import com.twu.biblioteca.myCommandInterface.MyCommand;
 import com.twu.biblioteca.repository.BookRepository;
+import com.twu.biblioteca.repository.MovieRepository;
 import com.twu.biblioteca.repository.Router;
 
+import com.twu.biblioteca.repository.UserRepository;
 import org.junit.Before;
 import org.junit.Test;
 import static  org.junit.Assert.assertEquals;
@@ -22,25 +23,53 @@ import java.util.Map;
 
 public class TestRouter {
     Router  router;
-    Map<String,MyCommand> COMMAND;
+    Map<String,MyCommand> FIRST_MENU;
+    Map<String,MyCommand> SECOND_MENU;
     BookRepository bookRepository;
-    List<Book> BOOK_LIST ;
+    MovieRepository movieRepository;
+    UserRepository userRepository;
+
     @Before
     public void setup(){
-        BOOK_LIST = new ArrayList<Book>();
-        COMMAND = new HashMap<String, MyCommand>(){
-            { put("1",new ListBooks(new BookRepository(BOOK_LIST))); }
-            { put("2",new CheckoutBook(new BookRepository(BOOK_LIST))); }
-            { put("3",new ReturnBook(new BookRepository(BOOK_LIST))); }
-            { put("4",new Quit()); }
+
+        bookRepository = new BookRepository(new ArrayList<Book>());
+        movieRepository = new MovieRepository(new ArrayList<Movie>());
+        userRepository = new UserRepository(new ArrayList<User>());
+
+        FIRST_MENU = new HashMap<String, MyCommand>(){
+            { put("1",new Login()); }
+            { put("2",new ListBooks(bookRepository)); }
+            { put("3",new ListMoviess(movieRepository)); }
+            { put("4",new MoreOption()); }
+            { put("5",new ExitLogin());}
+            { put("6",new Quit()); }
         };
-       // router = new Router(COMMAND);
-        bookRepository = new BookRepository(BOOK_LIST);
+
+        SECOND_MENU = new HashMap<String, MyCommand>(){
+            { put("1",new CheckoutBook(bookRepository)); }
+            { put("2",new ReturnBook(bookRepository)); }
+            { put("3",new CheckoutMovie(movieRepository));}
+            { put("4",new ReturnMovie(movieRepository)); }
+            { put("5",new UserAccounts(userRepository,bookRepository,movieRepository)); }
+            { put("6",new Return()); }
+        };
+
+        router = new Router(FIRST_MENU,SECOND_MENU);
+
+    }
+    @Test
+    public void should_return_Login_class_when_command_string_equal_1_and_current_command_is_FIRST_MENU(){
+        MyCommand myCommand = router.getCommand("1");
+        Login login = new Login();
+        String className = myCommand.getClass().getName();
+        String assertClassName = login.getClass().getName();
+
+        assertEquals(assertClassName,className);
     }
 
     @Test
-    public void should_return_ListBooks_class_when_command_string_equal_1(){
-        MyCommand myCommand = router.getCommand("1");
+    public void should_return_ListBooks_class_when_command_string_equal_2_and_current_command_is_FIRST_MENU(){
+        MyCommand myCommand = router.getCommand("2");
         ListBooks listBooks = new ListBooks(bookRepository);
         String className = myCommand.getClass().getName();
         String assertClassName = listBooks.getClass().getName();
@@ -49,28 +78,38 @@ public class TestRouter {
     }
 
     @Test
-    public void should_return_CkeckoutBook_class_when_command_string_equal_2(){
-        MyCommand myCommand = router.getCommand("2");
-        CheckoutBook checkoutBook = new CheckoutBook(bookRepository);
-        String className = myCommand.getClass().getName();
-        String assertClassName = checkoutBook.getClass().getName();
-
-        assertEquals(assertClassName,className);
-    }
-
-    @Test
-    public void should_return_ReturnBook_class_when_command_string_equal_3(){
+    public void should_return_ListMovies_class_when_command_string_equal_3_and_current_command_is_FIRST_MENU(){
         MyCommand myCommand = router.getCommand("3");
-        ReturnBook returnBook = new ReturnBook(bookRepository);
+        ListMoviess listMovies = new ListMoviess(movieRepository);
         String className = myCommand.getClass().getName();
-        String assertClassName = returnBook.getClass().getName();
+        String assertClassName = listMovies.getClass().getName();
 
         assertEquals(assertClassName,className);
     }
 
     @Test
-    public void should_return_ReturnBook_class_when_command_string_equal_4(){
+    public void should_return_MoreOption_class_when_command_string_equal_4_and_current_command_is_FIRST_MENU(){
         MyCommand myCommand = router.getCommand("4");
+        MoreOption moreOption = new MoreOption();
+        String className = myCommand.getClass().getName();
+        String assertClassName = moreOption.getClass().getName();
+
+        assertEquals(assertClassName,className);
+    }
+
+    @Test
+    public void should_return__ExitLogin_class_when_command_string_equal_5_and_current_command_is_FIRST_MENU(){
+        MyCommand myCommand = router.getCommand("5");
+        ExitLogin exitLogin = new ExitLogin();
+        String className = myCommand.getClass().getName();
+        String assertClassName = exitLogin.getClass().getName();
+
+        assertEquals(assertClassName,className);
+    }
+
+    @Test
+    public void should_return__Quit_class_when_command_string_equal_6_and_current_command_is_FIRST_MENU(){
+        MyCommand myCommand = router.getCommand("6");
         Quit quit = new Quit();
         String className = myCommand.getClass().getName();
         String assertClassName = quit.getClass().getName();
@@ -79,8 +118,32 @@ public class TestRouter {
     }
 
     @Test
+    public void should_return_CheckBook_class_when_command_string_equal_1_and_current_command_is_SECOND_MENU(){
+        router.currentComand = router.secondComand;
+        MyCommand myCommand = router.getCommand("1");
+        CheckoutBook checkoutBook = new CheckoutBook(bookRepository);
+        String className = myCommand.getClass().getName();
+        String assertClassName = checkoutBook.getClass().getName();
+
+        assertEquals(assertClassName,className);
+    }
+
+    @Test
+    public void should_return_ReturnBook_class_when_command_string_equal_2_and_current_command_is_SECOND_MENU(){
+        router.currentComand = router.secondComand;
+        MyCommand myCommand = router.getCommand("2");
+        ReturnBook returnBook = new ReturnBook(bookRepository);
+        String className = myCommand.getClass().getName();
+        String assertClassName = returnBook.getClass().getName();
+
+        assertEquals(assertClassName,className);
+    }
+
+
+
+    @Test
     public void should_return_null_when_command_string_not_exit(){
-        MyCommand myCommand = router.getCommand("5");
+        MyCommand myCommand = router.getCommand("9");
 
         assertNull(myCommand);
     }
--- test/com/twu/biblioteca/TestUserRepository.java
+++ test/com/twu/biblioteca/TestUserRepository.java
@@ -5,46 +5,74 @@ import com.twu.biblioteca.repository.UserRepository;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static  org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
 
 public class TestUserRepository {
     private UserRepository userRepository;
-
+    private List<User> USER_LIST;
 
     @Before
     public  void setUp(){
-       // userRepository = new UserRepository();
+        USER_LIST = new ArrayList<User>() {
+            {
+                add(new User("Lucy", "customer", "123-4567", "123@qq.com", "15100002222", "abc"));
+                add(new User("Jim", "customer", "456-7890", "456@qq.com", "18900001111", "def"));
+            }
+        };
+        userRepository = new UserRepository(USER_LIST);
     }
 
     @Test
-    public void should_return_user_islogin_true_with_login_successfully(){
+    public void should_return_user_when_call_login_with_login_successfully(){
         String libraryNumber = "123-4567";
         String password = "abc";
-       // User user = userRepository.login(libraryNumber,password);
+        User user = userRepository.login(libraryNumber,password);
 
-       // assertEquals(true,user.isLogin());
+        assertEquals("123-4567",user.getLibraryNumber());
+        assertEquals(true,user.isLogin());
     }
 
     @Test
-    public void should_return_user_islogin_false_with_login_successfully(){
+    public void should_return_null_when_call_login_with_wrong_library(){
         String libraryNumber = "1234567";
         String password = "abc";
-      //  User user = userRepository.login(libraryNumber,password);
+        User user = userRepository.login(libraryNumber,password);
 
-      //  assertEquals(null,user);
+        assertNull(user);
     }
 
     @Test
-    public void should_return_null_with_wrong_libraryNumber(){
-        String libraryNumber = "1234567";
+    public void should_return_null_when_call_login_with_wrong_password(){
+        String libraryNumber = "123-4567";
+        String password = "asd";
 
-        assertEquals(null,userRepository.getUserByLibraryNumber(libraryNumber));
+        assertEquals(null,userRepository.login(libraryNumber,password));
     }
 
     @Test
-    public void should_return_user_with_right_libraryNumber(){
+    public void should_return_user_when_call_getUserByLibraryNumber_with_right_libraryNumber(){
         String libraryNumber = "123-4567";
         User user = userRepository.getUserByLibraryNumber(libraryNumber);
-        assertEquals(true,user!=null);
+
+        assertEquals("123-4567",user.getLibraryNumber());
+        assertEquals("customer",user.getRole());
+        assertEquals("Lucy",user.getName());
+        assertEquals("123@qq.com",user.getEmail());
+        assertEquals("15100002222",user.getPhoneNumber());
+        assertEquals("abc",user.getPassword());
     }
+
+    @Test
+    public void should_return_null_when_call_getUserByLibraryNumber_with_wrong_libraryNumber(){
+        User user = userRepository.getUserByLibraryNumber("1234567");
+
+        assertNull(user);
+    }
+
+
 }
